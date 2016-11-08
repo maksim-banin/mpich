b03b8d687e4789285db9297061a28dd1bf970a89
Pavan Balaji <balaji@anl.gov>
2016-11-07 22:41:31 -0600

    Remove MPL_STATIC_INLINE_SUFFIX.
    The suffix is unnecessary to force static inlining.

--- src/mpl/include/mpl_base.h
+++ src/mpl/include/mpl_base.h
@@ -53,7 +53,6 @@
 #endif /* ATTRIBUTE */
 
 #define MPL_STATIC_INLINE_PREFIX ATTRIBUTE((always_inline)) static inline
-#define MPL_STATIC_INLINE_SUFFIX ATTRIBUTE((always_inline))
 
 /* These likely/unlikely macros provide static branch prediction hints to the
  * compiler, if such hints are available.  Simply wrap the relevant expression in
